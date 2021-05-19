7ceb266d0f631d561cf25a813d13d1b2a67d2e04
Ethan Huang <ethanh.os@gmail.com>
2021-05-19 10:09:25 -0400

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by user198878 on 5/19/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
